322840e4c3235795c7e69c5f1d8046e26d233fe3
vezhno <robofil190@gmail.com>
2025-04-21 17:11:33 +0300

    Task-3 added repository, view and controller for animated elements

---
+++ Controllers/AnimatedElementController.cs
@@ -0,0 +1,68 @@
+﻿using FHDatabase.Repositories;
+using FrontendHelper.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontendHelper.Controllers
+{
+    public class AnimatedElementController : Controller
+    {
+        private AnimatedElementRepository _animatedElementRepository;
+
+        public AnimatedElementController(AnimatedElementRepository animatedElementRepository)
+        {
+            _animatedElementRepository = animatedElementRepository;
+        }
+
+
+        // показ всех иконок (без фильтра)
+        public IActionResult ShowAllAnimatedElements()
+        {
+            var iconDatas = _animatedElementRepository.GetAssets();
+            var viewModels = iconDatas.Select(PassDataToViewModel).ToList();
+            return View(viewModels);
+        }
+
+
+
+        // показ всех иконок по определенной теме
+        public IActionResult ShowAllAnimatedElementsOnTheTopic(string topic)
+        {
+            var viewModels = _animatedElementRepository
+                .GetAllAnimatedElementsByTopic(topic)
+                .Select(PassDataToViewModel)
+                .ToList();
+
+            ViewBag.Topic = topic;   // для вывода темы во вьюшке, ПОТОМ УБЕРУ
+            return View(viewModels);
+        }
+
+
+
+        // показ групп иконок по всем темам
+        public IActionResult ShowGroupsOfAnimatedElementsOnTheTopic(int numberOfIcons = 6)
+        {
+            var iconTopics = _animatedElementRepository.GetAnimatedElementTopics();
+
+            var viewModels = iconTopics.Select(topic => new AnimatedElementGroupViewModel
+            {
+                Topic = topic,
+                AnimatedElements = _animatedElementRepository
+                .GetAnimatedElementGroupByTopic(topic, numberOfIcons)
+                .Select(PassDataToViewModel)
+                .ToList()
+            });
+
+            return View(viewModels);
+        }
+        private AnimatedElementViewModel PassDataToViewModel(AnimatedElementData elementData)
+        {
+            return new AnimatedElementViewModel
+            {
+                Id = elementData.Id,
+                Name = elementData.Name,
+                Img = elementData.Img,
+                Topic = elementData.Topic
+            };
+        }
+    }
+}
---
+++ Models/AnimatedElementGroupViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FrontendHelper.Models
+{
+    public class AnimatedElementGroupViewModel
+    {
+        public string Topic { get; set; }
+        public List<AnimatedElementViewModel> AnimatedElements { get; set; }
+    }
+}
