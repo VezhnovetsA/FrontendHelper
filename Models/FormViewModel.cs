30612c2ba8dc3abe5f919f65c4c6eb95afd6dcbd
vezhno <robofil190@gmail.com>
2025-04-28 01:37:31 +0300

    Task-8 added controller, viewmodel, view and html-files for forms

---
+++ Models/FormViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FrontendHelper.Models
+{
+    public class FormViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string FormCode { get; set; }
+
+    }
+}
