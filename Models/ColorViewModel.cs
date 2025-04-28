dabbfef0107d1df29cebe3972cf68056f0ba93bc
vezhno <robofil190@gmail.com>
2025-04-29 01:12:50 +0300

    Task-10 Added data model, view models, controller, repository and views for palette

---
+++ Models/ColorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FrontendHelper.Models
+{
+    public class ColorViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Hex { get; set; }
+    }
+}
